1b1c287b6bb26d8aac613f57c3b80eb152b5f1c1
igor-brishkoski <igor.brishkoski@gmail.com>
2015-06-06 23:05:18 -0400

    moving the cart into the sidebar

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,7 +32,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         session[:counter] = 0
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_url }
         format.json { render :show, status: :created, location: @line_item.cart }
       else
         format.html { render :new }
