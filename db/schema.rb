1842288743af30f5231af693ce5a3938065cc6eb
igor-brishkoski <igor.brishkoski@gmail.com>
2015-10-04 17:56:12 -0400

    testing buildpack db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151004204906) do
+ActiveRecord::Schema.define(version: 20151004215324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20151004204906) do
     t.decimal  "price",                   precision: 8, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "test_name"
   end
 
   add_foreign_key "line_items", "orders"
